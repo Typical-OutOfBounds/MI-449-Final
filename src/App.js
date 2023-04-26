80bdf31f7e024b35f84a16561d3bc0459e8df632
Aidan Lane <aidanlane0@protonmail.com>
2023-04-26 12:33:01 -0400

    under construction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Under Construction
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://comartsci.msu.edu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
